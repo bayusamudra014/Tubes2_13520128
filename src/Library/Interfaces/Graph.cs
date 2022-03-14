966273aa2668baf8028e9810427e2282ce736cfe
ghaziankamil <16520465@std.stei.itb.ac.id>
2022-03-14 19:14:05 +0700

    test fungsi graph

--- src/Library/Interfaces/Graph.cs
+++ src/Library/Interfaces/Graph.cs
@@ -1,4 +1,6 @@
 namespace PathFinder.Interfaces
+    // template buat fungsi template graph, buat nyimpen data node dan juga posisinya
+    // saat ini lagi dimana
 {
     public interface Graph<T>
     {
