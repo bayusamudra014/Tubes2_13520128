8343e7bcf139db8f5b5f311ff4ffbdeac6068948
ghaziankamil <16520465@std.stei.itb.ac.id>
2022-03-16 12:13:14 +0700

    struktur data tree done

--- src/Library/Interfaces/Graph.cs
+++ src/Library/Interfaces/Graph.cs
@@ -16,7 +16,8 @@ using System.Collections.Generic;
 
 // bikin data struktur buat tree nya
 
-public class TreeNode<T> {
+public class TreeNode<T>
+{
 
     private T value;
 
@@ -25,12 +26,101 @@ public class TreeNode<T> {
 
     private List<TreeNode<T>> children;
 
-    public TreeNode(T value) { 
-        if (value == null) {
+    public TreeNode(T value)
+    {
+        if (value == null)
+        {
             throw new ArgumentNullException("Cannot insert null value!");
         }
         this.value = value;
         this.children = new List<TreeNode<T>>();
+    }
 
+    public T Value { 
+    
+        get { 
+            return value; 
+        }
+        set { 
+            this.value = value; 
+        }
+        
+    }
+
+    public int ChildrenCount {
+
+        get { 
+            return this.children.Count;
+        }
+    
+    }
+
+    public void AddChild(TreeNode<T> child) {
+
+        if (child == null) { 
+            
+            throw new ArgumentNullException("Cannot insert null value!");
+        
+        }
+
+        if (child.hasParent) {
+
+            throw new ArgumentException("The node already has a parent!");
+        
+        }
+
+        child.hasParent = true;
+        this.children.Add(child);
+    
+    }
+
+    public TreeNode<T> GetChild(int index) { 
+        
+        return this.children[index];
+        
+    }
 
 }
+
+public class Tree<T> {
+    
+    private TreeNode<T> root;
+
+
+    public Tree(T value) {
+
+        if (value == null) {
+
+            throw new ArgumentNullException("Cannot insert null value!");
+        
+        }
+
+        this.root = new TreeNode<T>(value);
+    
+    }
+
+
+    public Tree(T value, params Tree<T>[] children) : this(value)
+    {
+
+        foreach (Tree<T> child in children)
+        {
+
+            this.root.AddChild(child.root);
+
+        }
+    }
+
+    public TreeNode<T> Root {
+
+        get {
+            
+            return this.root;
+        
+        }
+    
+    }
+
+}
+
+
